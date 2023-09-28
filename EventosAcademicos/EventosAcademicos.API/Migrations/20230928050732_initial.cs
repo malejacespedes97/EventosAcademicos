8c850c40acb3eb7b6084ca1ea7e53a37e7804d4c
María Alejandra Correa <malejacespedes@gmail.com>
2023-09-28 00:22:49 -0500

    Migración y Repositorios

---
+++ EventosAcademicos/EventosAcademicos.API/Migrations/20230928050732_initial.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventosAcademicos.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EventosAcademicos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FechaInicio = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    FechaFinal = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Ubicacion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    TemaEvento = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventosAcademicos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Participantes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Afiliacion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    AreaInteres = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    TipoParticipacion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participantes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventosAcademicos");
+
+            migrationBuilder.DropTable(
+                name: "Participantes");
+        }
+    }
+}
--- EventosAcademicos/EventosAcademicos.WEB/Program.cs
+++ EventosAcademicos/EventosAcademicos.WEB/Program.cs
@@ -1,11 +1,17 @@
 using EventosAcademicos.WEB;
+using CurrieTechnologies.Razor.SweetAlert2;
+using EventosAcademicos.WEB.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000/") }
+
+builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
