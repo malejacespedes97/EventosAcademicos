82ce282e2ebfc1b5af038c48938456f7c1615b61
María Alejandra Correa <malejacespedes@gmail.com>
2023-09-27 22:40:20 -0500

    Proyecto base, creación de entidades y conexión a BD

---
+++ EventosAcademicos/EventosAcademicos.API/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using EventosAcedemicos.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace EventosAcademicos.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        //Notación diamante es el nombre de la entidad <>, luego de esto se debe agregar la misma clase en plural
+        public DbSet<EventoAcademico> EventosAcademicos { get; set; } 
+
+        public DbSet<Participante> Participantes { get; set; } 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
