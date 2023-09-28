f688d780a7ad93e9df6e911d030bcaf6f65a3747
María Alejandra Correa <malejacespedes@gmail.com>
2023-09-28 02:21:17 -0500

    Implementación de vistas

--- EventosAcademicos/EventosAcademicos.WEB/Program.cs
+++ EventosAcademicos/EventosAcademicos.WEB/Program.cs
@@ -3,13 +3,12 @@ using CurrieTechnologies.Razor.SweetAlert2;
 using EventosAcademicos.WEB.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000/") }
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000/") }//inyección del Repository
 
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
